c9a489084395050e7e1b4d11de0d672bbe481372
juniair.kim <kkj@codemind.co.kr>
2025-05-02 15:39:25 +0900

    feature: DTO 단위테스트 코드 작성
    - 롬복을 활용하여 DTO 생성자, Getter를 어노테이션으로 지정
    - HelloReponseDTO 클래스 정의
    - HelloResponseDTO 클래스에 대한 단위테스트 클래스 정의

---
+++ src/test/java/com/juniair/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,26 @@
+package com.juniair.springboot.web.dto;
+
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void lombokTest() {
+
+        // given
+        String name = "test";
+        int amount = 100;
+
+        // when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        // that
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+
+    }
+
+}
