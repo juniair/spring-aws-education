e772165e79bf4f6b797707646459ca1d03ee13be
juniair.kim <kkj@codemind.co.kr>
2025-05-02 16:36:18 +0900

    feature: Posts 서비스에 기능 추가
    - Posts 업데이트 요청을 처리하기 위한 update 메서드 추가
    - 업데이트핳 Posts를 찾기위한 findById 메서드 추가

--- src/main/java/com/juniair/springboot/service/posts/PostsService.java
+++ src/main/java/com/juniair/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.juniair.springboot.service.posts;
 
+import com.juniair.springboot.domain.posts.Posts;
 import com.juniair.springboot.domain.posts.PostsRepository;
+import com.juniair.springboot.web.dto.PostsResponseDto;
 import com.juniair.springboot.web.dto.PostsSaveRequestDto;
+import com.juniair.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,6 +19,21 @@ public class PostsService {
     @Transactional
     public long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new
+                IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById (Long id){
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new
+                IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        return new PostsResponseDto(entity);
     }
 
 }
